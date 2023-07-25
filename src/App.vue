757af230f1d9ee67e4e364662ba4cf98ed91e94f
radhifazl <radhifazlinurfahriza@gmail.com>
2023-07-25 07:34:57 +0700

    Add SASS Variables

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <h1 id="tagline">Hello</h1>
     <router-view/>
   </div>
 </template>
@@ -14,7 +11,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primary-color;
 }
 
 nav {
